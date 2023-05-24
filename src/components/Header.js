4e805d4cb56c48c9df8f3188e6e865da889e63fe
LucasNahuel <munozlucas96@gmail.com>
2023-05-23 23:14:46 -0300

    react project initialized and basic layout

---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+
+import Score from './Score';
+
+function Header(){
+
+    return(
+        <div className="header">
+            <ul className="title-list">
+                <li>ROCK</li>
+                <li>PAPER</li>
+                <li>SCISSORS</li>
+                <li>LIZARD</li>
+                <li>SPOCK</li>
+            </ul>
+
+            <Score/>
+        </div>
+    );
+}
+
+export default Header;
---
+++ src/components/Score.js
@@ -0,0 +1,17 @@
+
+
+function Score(){
+    return(
+        <div className="score-container">
+            <p className="score-title">
+                SCORE
+            </p>
+
+            <p className="score-value">
+                12
+            </p>
+        </div>
+    );
+}
+
+export default Score;
